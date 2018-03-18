4a32c145e2ed6b226b6dc39e3db858f892596c59
tengfei <langerfei@163.com>
2018-03-18 16:14:22 +0800

    yii

---
+++ basic/controllers/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+	namespace app\controllers;
+	use yii\web\Controller;
+	
+	/**
+	* 
+	*/
+	class HelloController extends Controller
+	{
+		public function actionOne(){
+			echo 'hello word';
+		}
+	}
+ ?>
